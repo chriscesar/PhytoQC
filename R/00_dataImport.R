ec8d1ef2f477944e8ab5e8ec99d14415e9a62bd2
Christopher Cesar <chris.cesar@environment-agency.gov.uk>
2023-10-06 11:13:04 +0100

    Rename SampleID variables to maintain correct order. Removed extract file name

--- R/00_dataImport.R
+++ R/00_dataImport.R
@@ -17,22 +17,21 @@ excel_files <-
 
 # Function to read specific cells from a workbook and associate with filename
 read_excel_data <- function(file_path) {
-  # Get the filename with the extension
-  file_name <- basename(file_path)
+  # # Get the filename with the extension
+  # file_name <- basename(file_path)
   
   # Read cells A1:B13 from the worksheet "Sample_Details"
-  sample_details <- readxl::read_excel(
-    file_path,
-    sheet = "Sample_Details",
-    range = "A1:B13",
-    col_names = FALSE
-  )
+  sample_details <- readxl::read_excel(file_path,
+                                       sheet = "Sample_Details",
+                                       range = "A1:B13",
+                                       col_names = FALSE)
   
   # Assign column names to sample_details
   colnames(sample_details) <- c("Variable", "Value")
   
   # Handle potentially blank cells and convert them to NA
-  sample_details$Value <- ifelse(sample_details$Value == "", NA, sample_details$Value)
+  sample_details$Value <-
+    ifelse(sample_details$Value == "", NA, sample_details$Value)
   
   # Read additional values from Input_Data sheet (cells C1 and C2)
   water_sample_vol <- readxl::read_excel(
@@ -58,8 +57,28 @@ read_excel_data <- function(file_path) {
     c("SubSampleVol_ml", as.character(sub_sample_vol[[1]]))
   )
   
+  # Rename rows to maintain correct order
+  sample_details[, 1] <- c(
+    "01_AnaylsisLab",
+    "02_LabSwap",
+    "03_OriginalAnalyst",
+    "04_AnalysisDate_Orig",
+    "05_NameOfSurvey_WFD",
+    "06_SampleDate",
+    "07_EAOldSiteCode",
+    "08_EAWIMSCode",
+    "09_InternalSampleID",
+    "10_AuditAnalyst*",
+    "11_AuditDate_BaseRec",
+    "12_AuditDate_RepSub",
+    "13_Comments",
+    "14_WaterSampleVol_ml",
+    "15_SubSampleVol_ml"
+  )
+  
   # Convert sample_details to wide format
-  sample_details <- spread(sample_details, key = Variable, value = Value)
+  sample_details <-
+    spread(sample_details, key = Variable, value = Value)
   
   # Read cells B6:I238 from the worksheet "Input_Data" as character values
   input_data <- readxl::read_excel(
@@ -122,15 +141,14 @@ read_excel_data <- function(file_path) {
     filter(!(is.na(dens)) & !(dens == 0))
   
   # Read the "QA_Summary" table from cells A1:B5
-  qa_summary <- readxl::read_excel(
-    file_path,
-    sheet = "QA_Summary",
-    range = "A1:B5",
-    col_names = TRUE
-  )
+  qa_summary <- readxl::read_excel(file_path,
+                                   sheet = "QA_Summary",
+                                   range = "A1:B5",
+                                   col_names = TRUE)
   
   # Rename specific row values in qa_summary
-  qa_summary[, 1] <- c("QA1_TotAbund", "QA2_DomTax", "QA3_SharedTax", "QA4_Final")
+  qa_summary[, 1] <-
+    c("QA1_TotAbund", "QA2_DomTax", "QA3_SharedTax", "QA4_Final")
   
   # Convert qa_summary to wide format
   qa_summary <- spread(qa_summary, key = Test, value = Outcome)
