e80c6132f703b3e5c902a4dde2d263ab8f421573
Christopher Cesar <chris.cesar@environment-agency.gov.uk>
2023-11-24 13:37:19 +0000

    Load pre-exported data

--- R/00phyto.ords_00.R
+++ R/00phyto.ords_00.R
@@ -16,22 +16,23 @@ perms <- 9999 #number of permutations of analyses
 theme_set(ggthemes::theme_few())###set theme for all ggplot objects
 
 # load data 
-df0 <- as_tibble(readxl::read_xlsx("data/Copy of PhytoChl_2000-2020 (WB+RegionalSeas).xlsx",
-                                   sheet = "Abundance2000_2020USE"))
-
-# filter, convert to wide to prep for ordination ####
-df0 %>% 
-  dplyr::select(.,-aphiaID, -Time) %>% #remove unneccessary cols
-  filter(Year >= 2012) %>%  ##keep only samples gathered from 2012 onwards
-  #drop NA values in TypologyCode to keep only WBs selected for analysis
-  filter(!is.na(TypologyCode)) %>% 
-  # names(.)
-  # group_by(across(c(-Abundance))) %>% count(.) %>% View(.)
-  pivot_wider(names_from = TaxonUSE,
-              values_from = Abundance,
-              values_fill=0) -> dfw
-
-saveRDS(dfw, file = "data/out/phyto_WBs_sideProj.Rdat")
+# df0 <- as_tibble(readxl::read_xlsx("data/Copy of PhytoChl_2000-2020 (WB+RegionalSeas).xlsx",
+#                                    sheet = "Abundance2000_2020USE"))
+# 
+# # filter, convert to wide to prep for ordination ####
+# df0 %>% 
+#   dplyr::select(.,-aphiaID, -Time) %>% #remove unneccessary cols
+#   filter(Year >= 2012) %>%  ##keep only samples gathered from 2012 onwards
+#   #drop NA values in TypologyCode to keep only WBs selected for analysis
+#   filter(!is.na(TypologyCode)) %>% 
+#   # names(.)
+#   # group_by(across(c(-Abundance))) %>% count(.) %>% View(.)
+#   pivot_wider(names_from = TaxonUSE,
+#               values_from = Abundance,
+#               values_fill=0) -> dfw
+# 
+# saveRDS(dfw, file = "data/out/phyto_WBs_sideProj.Rdat")
+dfw <- readRDS("data/out/phyto_WBs_sideProj.Rdat")
 
 ## ordinate data ###
 ## remove metadata
