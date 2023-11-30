17cdfd8ba7dad4c6d7d756b2c030d05f968f2e3a
Christopher Cesar <3082834+chriscesar@users.noreply.github.com>
2023-11-30 15:00:42 +0000

    Update for onscreen reading

--- R/00_dataImport.R
+++ R/00_dataImport.R
@@ -24,13 +24,12 @@ excel_files <-
 # Function to read specific cells from a workbook and associate with filename
 read_excel_data <- function(file_path) {
   
-  ## Extract info from Sample_Details worksheet ##
+  ## Extract info from ###-Sample_Details-### worksheet ##
 
   # Read cells A1:B13 from the worksheet "Sample_Details"
   sample_details <- readxl::read_excel(file_path,
                                        sheet = "Sample_Details",
-                                       range = "A1:B13",
-                                       col_names = FALSE)
+                                       range = "A1:B13", col_names = FALSE)
   
   # Assign column names to sample_details
   colnames(sample_details) <- c("Variable", "Value")
@@ -42,18 +41,14 @@ read_excel_data <- function(file_path) {
   # Read additional values from Input_Data sheet (cells C1 and C2)
   water_sample_vol <- readxl::read_excel(
     file_path,
-    sheet = "Input_Data",
-    range = "C1",
-    col_names = FALSE,
-    col_types = "text"
+    sheet = "Input_Data", range = "C1",
+    col_names = FALSE, col_types = "text"
     )
   
   sub_sample_vol <- readxl::read_excel(
     file_path,
-    sheet = "Input_Data",
-    range = "C2",
-    col_names = FALSE,
-    col_types = "text"
+    sheet = "Input_Data", range = "C2",
+    col_names = FALSE, col_types = "text"
     )
   
   # Add rows for WaterSampleVol_ml and SubSampleVol_ml to sample_details
@@ -65,15 +60,14 @@ read_excel_data <- function(file_path) {
   
   # Rename rows to maintain correct order
   sample_details[, 1] <- c(
-    "SD01_AnaylsisLab", "SD02_LabSwap",
-    "SD03_OriginalAnalyst", "SD04_AnalysisDateOrig",
-    "SD05_NameOfSurvey_WFD", "SD06_SampleDate",
-    "SD07_EAOldSiteCode", "SD08_EAWIMSCode",
-    "SD09_InternalSampleID", "SD10_AuditAnalyst",
-    "SD11_AuditDateBaseRec", "SD12_AuditDateRepSub",
-    "SD13_Comments", "SD14_WaterSampleVol_ml",
-    "SD15_SubSampleVol_ml"
-    )
+         "SD01_AnaylsisLab", "SD02_LabSwap", "SD03_OriginalAnalyst",
+         "SD04_AnalysisDateOrig", "SD05_NameOfSurvey_WFD",
+         "SD06_SampleDate", "SD07_EAOldSiteCode",
+         "SD08_EAWIMSCode", "SD09_InternalSampleID",
+         "SD10_AuditAnalyst", "SD11_AuditDateBaseRec",
+         "SD12_AuditDateRepSub", "SD13_Comments",
+         "SD14_WaterSampleVol_ml", "SD15_SubSampleVol_ml"
+         )
   
   # Convert sample_details to wide format
   sample_details <-
@@ -85,72 +79,70 @@ read_excel_data <- function(file_path) {
   # Add the file_name to the sample_details data frame
   sample_details <- cbind("SD00FileName" = file_name, sample_details)
   
-  ## Extract info from Input_Data worksheet ##
+  ## Extract info from ###-Input_Data-### worksheet ##
   
   # Read cells B6:I238 from the worksheet "Input_Data" as character values
-  input_data <- readxl::read_excel(
-    file_path,
-    sheet = "Input_Data",
-    range = "B6:I238",
-    col_names = FALSE, col_types = "text"
-    )
+       input_data <- readxl::read_excel(
+         file_path,
+         sheet = "Input_Data", range = "B6:I238",
+         col_names = FALSE, col_types = "text"
+         )
   
   # Assign custom column names to input_data
-  colnames(input_data) <- c(
-    "Taxon", "Qualifier",
-    "Original_dens", "Baseplate_dens",
-    "Replicate_dens", "Original_prop",
-    "Baseplate_prop", "Replicate_prop"
-    )
+       colnames(input_data) <- c(
+         "Taxon", "Qualifier",
+         "Original_dens", "Baseplate_dens",
+         "Replicate_dens", "Original_prop",
+         "Baseplate_prop", "Replicate_prop"
+         )
   
   # Concatenate "Taxon" and "Qualifier" into a new variable "Tax_Qual"
-  input_data$Tax_Qual <- ifelse(
-    !is.na(input_data$Qualifier),
-    paste(input_data$Taxon, input_data$Qualifier, sep = "_"),
-    input_data$Taxon
-    )
+       input_data$Tax_Qual <- ifelse(
+         !is.na(input_data$Qualifier),
+         paste(input_data$Taxon, input_data$Qualifier, sep = "_"),
+         input_data$Taxon
+         )
   
   # Remove "Taxon" and "Qualifier" variables
   input_data <- input_data %>%
-    select(
-      Tax_Qual, Original_dens,
-      Baseplate_dens, Replicate_dens,
-      Original_prop, Baseplate_prop,
-      Replicate_prop
-    ) %>%
+       select(
+              Tax_Qual, Original_dens,
+              Baseplate_dens, Replicate_dens,
+              Original_prop, Baseplate_prop,
+              Replicate_prop
+              ) %>%
     # Remove rows where all count values are NA or zero
     filter(
-      !(
-        is.na(Original_dens) &
-          is.na(Baseplate_dens) & is.na(Replicate_dens) &
-          is.na(Original_prop) &
-          is.na(Baseplate_prop) & is.na(Replicate_prop)
-      ) &
-        !(
-          Original_dens == 0 & Baseplate_dens == 0 & Replicate_dens == 0 &
-            Original_prop == 0 &
-            Baseplate_prop == 0 & Replicate_prop == 0
-        )
-    ) %>%
+           !(
+                  is.na(Original_dens) &
+                  is.na(Baseplate_dens) & is.na(Replicate_dens) &
+                  is.na(Original_prop) &
+                  is.na(Baseplate_prop) & is.na(Replicate_prop)
+                  ) &
+           !(
+                  Original_dens == 0 & Baseplate_dens == 0 & Replicate_dens == 0 &
+                  Original_prop == 0 &
+                  Baseplate_prop == 0 & Replicate_prop == 0
+                  )
+           ) %>%
        ##convert count data to long format
-    pivot_longer(-Tax_Qual,
-                 names_to = c("AnalysisType", ".value"),
-                 names_sep = "_") %>%
-    #remove NA or 0 values
-    filter(!(is.na(dens)) & !(dens == 0))
+       pivot_longer(-Tax_Qual,
+                    names_to = c("AnalysisType", ".value"),
+                    names_sep = "_") %>%
+       #remove NA or 0 values
+       filter(!(is.na(dens)) & !(dens == 0))
   
-  ## Extract info from QA_Summary worksheet ##
+  ## Extract info from ###-QA_Summary-### worksheet ##
   
   # Read the "QA_Summary" table from cells A1:B5
-  qa_summary <- readxl::read_excel(file_path,
-                                   sheet = "QA_Summary",
-                                   range = "A1:B5",
-                                   col_names = TRUE)
+       qa_summary <- readxl::read_excel(file_path,
+                                        sheet = "QA_Summary", range = "A1:B5",
+                                        col_names = TRUE)
   
   # Rename specific row values in qa_summary
   qa_summary[, 1] <-
-    c("QA01_TotAbund", "QA02_DomTax",
-      "QA03_SharedTax", "QA04_Final")
+       c("QA01_TotAbund", "QA02_DomTax",
+         "QA03_SharedTax", "QA04_Final")
   
   # Convert qa_summary to wide format
   qa_summary <- spread(qa_summary, key = Test, value = Outcome)
