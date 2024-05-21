967de42263312c11c920084ade3820a7d0450cf4
chriscesar <chris.cesar@environment-agency.gov.uk>
2024-05-21 15:46:03 +0100

    Model running and exporting

--- R/101_init_data_expl.R
+++ R/101_init_data_expl.R
@@ -3,26 +3,22 @@
 
 # admin ####
 # load packages #
-ld_pkgs <- c("tidyverse","ggthemes","vegan", "rgl", "mvabund")
+ld_pkgs <- c("tidyverse","vegan", "rgl", "mvabund","tictoc")
 vapply(ld_pkgs, library, logical(1L),
        character.only = TRUE, logical.return = TRUE)
 rm(ld_pkgs)
 
-# set themes/global presets
-cbPalette <- c("#0072B2","#e79f00","#009E73", "#9ad0f3", "#000000",
-               "#D55E00", "#CC79A7", "#F0E442")
-ppi <- 300
-perms <- 9999 #number of permutations of analyses
-theme_set(ggthemes::theme_few())###set theme for all ggplot objects
-
+tictoc::tic.clearlog();tic("TOTAL TIME")
+tic("ADMIN: Set metadata; load data")
+source("R/00_setMeta.R")
 # load data 
-df0 <- as_tibble(read.csv("data/out/extracted_data_NonLabSwap.csv"))
+df0 <- as_tibble(read.csv("data_processed/extracted_data_NonLabSwap.csv"))
 
 # Widen data and run MDS ####
 
 ### filter by AnalysisType = "Original" & widen
 df0_orig_w <- df0 %>% 
-  filter(.,!is.na(SD00FileName)) %>% 
+  filter(.,!is.na(SD00_FileName)) %>% 
   filter(AnalysisType == "Original") %>% 
   dplyr::select(., # remove unnecessary columns
                 !c(prop,
@@ -31,7 +27,7 @@ df0_orig_w <- df0 %>%
                    QA03_SharedTax,
                    QA04_Final)) %>% 
   dplyr::group_by(
-    SD00FileName,
+    SD00_FileName,
     SD01_AnaylsisLab,
     SD02_LabSwap,
     SD03_OriginalAnalyst,
@@ -54,16 +50,23 @@ df0_orig_w <- df0 %>%
               values_fill = 0) %>% # widen data
   ungroup()
 
+## save wide data ###
+write.csv(df0_orig_w, file="data_processed/extracted_data_NonLabSwap_wide.csv")
+toc(log=TRUE)
+
+tic("Run ordination")
 ### create temporary file for ordination
 # remove metadata
 dftmp <- df0_orig_w %>% 
-  dplyr::select(!c(SD00FileName:AnalysisType))
+  dplyr::select(!c(SD00_FileName:AnalysisType))
 set.seed(pi);ord <- vegan::metaMDS(dftmp,
                       try = 20,
                       trymax = 1000,
                       k = 3)
 #plot(ord)
+toc(log=TRUE)
 
+tic("Plot ordination using ggplot")
 ## extract data for ggplot2 ####
 #### extract ordination axes ####
 scores_site <- df0_orig_w %>% 
@@ -77,7 +80,7 @@ unq <- levels(as.factor(scores_site$SD01_AnaylsisLab)) ## extract unique Lab val
 scores_site$LabCol <- case_when(scores_site$SD01_AnaylsisLab == unq[1] ~ cbPalette[1],
                                 scores_site$SD01_AnaylsisLab == unq[2] ~ cbPalette[2],
                                    TRUE ~ NA_character_)
-saveRDS(scores_site, file = "data/out/scores_site3d.Rdata")
+saveRDS(scores_site, file = "data_processed/scores_site3d.Rdata")
 rm(tmp_sites)
 
 # Using the scores function from vegan to extract the species scores and
@@ -87,7 +90,7 @@ scores_species$lbfull <-  row.names(scores_species)
 scores_species$lb <-  make.cepnames(row.names(scores_species))#shorten names
 scores_species$lb <- gsub("µ","u",scores_species$lb)
 
-saveRDS(scores_species, file = "data/out/scores_species3d.Rdata")
+saveRDS(scores_species, file = "data_processed/scores_species3d.Rdata")
 
 plot3d(x = scores_site$NMDS1,
        y = scores_site$NMDS2,
@@ -106,23 +109,51 @@ axes3d();title3d(xlab="NMDS1",
                  ylab="NMDS2",
                  zlab="NMDS3",
                  font=2)
-
+toc(log=TRUE)
 
 # statistical comparisons ####
 ### do assemblages differ by lab?
 ## vegan version
+tic("Run 1 way PERMANOVA")
 (fit01 <- vegan::adonis2(dftmp ~ df0_orig_w$SD01_AnaylsisLab,
                          permutations = perms))
+toc(log=TRUE)
 
 ## mvabund
+tic("Run mvabund model: raw counts")
 mvdftmp <- mvabund(dftmp)
-fit02 <- manyglm(mvdftmp ~ df0_orig_w$SD01_AnaylsisLab)
-# anova_fit02 <- mvabund::anova.manyglm(fit02,p.uni = "adjusted")
+fit_raw <- manyglm(mvdftmp ~ df0_orig_w$SD01_AnaylsisLab,family="negative.binomial")
+anova_fit_raw <- mvabund::anova.manyglm(fit_raw,p.uni = "adjusted")
+toc(log=TRUE)
+
+tic("Run mvabund model: sqrt transformed")
+fit_sqrt <- manyglm(sqrt(mvdftmp) ~ df0_orig_w$SD01_AnaylsisLab,family="negative.binomial")
+anova_fit_sqrt <- mvabund::anova.manyglm(fit_sqrt,p.uni = "adjusted")
+toc(log=TRUE)
 
 ### mvabund based on presence/absence data
+tic("Run mvabund model: binary")
 dftmp_bin <- dftmp
 dftmp_bin[dftmp_bin>0] = 1
 mvdftmp_bin <- mvabund(dftmp_bin)
+fit_bin <- manyglm(mvdftmp_bin ~ df0_orig_w$SD01_AnaylsisLab, family=binomial())
+anova_fit_bin <- mvabund::anova.manyglm(fit_bin,p.uni = "adjusted")
+toc(log=TRUE)
+
+## save outputs
+saveRDS(fit_raw, file = "model_outputs/fit_raw_model.Rdat")
+saveRDS(anova_fit_raw, file = "model_outputs/fit_raw_model_anova.Rdat")
+saveRDS(fit_sqrt, file = "model_outputs/fit_sqrt_model.Rdat")
+saveRDS(anova_fit_sqrt, file = "model_outputs/fit_sqrt_model_anova.Rdat")
+saveRDS(fit_bin, file = "model_outputs/fit_bin_model.Rdat")
+saveRDS(anova_fit_bin, file = "model_outputs/fit_bin_model_anova.Rdat")
+
+(x <- unlist(tictoc::tic.log()))
+saveRDS(x,file = "model_outputs/model_times.Rdat")
+########################################
+########################################
+########################################
+
 # summary(fit03 <- manyglm(mvdftmp_bin~df0_orig_w$SD01_AnaylsisLab,
 #                  family="binomial"))
 # anova_fit03 <- mvabund::anova.manyglm(fit03,
